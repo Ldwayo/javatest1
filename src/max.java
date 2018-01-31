9e714f5dcfaff75d913957e8585708dc15aebee7
Ldwayo <lindongwangyu@qq.com>
2018-01-31 11:40:02 +0800

    01背包问题2

--- src/max.java
+++ src/max.java
@@ -9,4 +9,9 @@ public class max {      //max help
             return a;
         else return b;
     }
+    public int min(){
+        if(a<b)
+            return a;
+        else return b;
+    }
 }
